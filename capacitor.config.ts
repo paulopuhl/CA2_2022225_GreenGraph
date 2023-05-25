dd02369a04fb1660c864eed24a11e6c2db7fb768
paulopuhl <paulopuhl@gmail.com>
2023-05-25 19:47:45 +0100

    Starting project

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CA2_2022225_GreenGraph',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
